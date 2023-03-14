6f95c282bfdc87cf5bb2c0aded134b3764e762fd
valent33 <v.porchet@outlook.fr>
2023-03-14 16:23:07 -0400

    décomposition packet

--- test.py
+++ test.py
@@ -7,9 +7,18 @@ def capture_packets():
     asyncio.set_event_loop(loop)
 
     cap = pyshark.FileCapture('Données\mdk3_1h20.pcap')
-    for packet in cap:
-        print(packet)
+    for i, p in enumerate(cap):
+        for layer in p.layers:
+            print("###############", layer.layer_name, "###############")
+            for field in layer.field_names:
+                print(field, getattr(layer, field))
+        
+        # print(p)
+        print(i)
         break
+
+    # Close the capture object
+    cap.close()
 
 if __name__ == '__main__':
     t = threading.Thread(target=capture_packets)
