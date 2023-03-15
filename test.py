4c0f59334f38f0694e349cfba8333257de00ec43
valent33 <v.porchet@outlook.fr>
2023-03-15 09:35:36 -0400

    csv version bruteforce

--- test.py
+++ test.py
@@ -4,19 +4,28 @@ import threading
 import pandas as pd
 
 df = pd.DataFrame(columns=["packet_length", "header_length", "subtype", "duration", "datarate", "class"])
+# write header to csv
+df.to_csv("Données\\bruteforce_12min.csv", mode='w', header=True)
 
 def capture_packets():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    cap = pyshark.FileCapture('Données\mdk3_1h20.pcap')#, display_filter='frame.number > 170000')
+    # cap = pyshark.FileCapture('Données\mdk3_1h20.pcap')#, display_filter='frame.number > 170000')
     
-    # frame 177229 attack started
-    start_frame = 177229
-    # frame 184365 attack terminated
-    end_frame = 184365
+    # # frame 177229 attack started
+    # start_frame = 177229
+    # # frame 184365 attack terminated
+    # end_frame = 184365
     
-    # cap = pyshark.FileCapture('Données\\bruteforce_12min.pcap')
+    cap = pyshark.FileCapture('Données\\bruteforce_12min.pcap')
+
+    # frame  attack started
+    start_frame = 48794
+    # frame  attack terminated
+    end_frame = 166490
+    
+    # cap = pyshark.FileCapture('Données\\attaqueslowloris_35min.pcapng')
 
     # # frame  attack started
     # start_frame = 1
@@ -41,12 +50,14 @@ def capture_packets():
         df.loc[i, "datarate"] = p.wlan_radio.data_rate # Mbps
         
         if i >= start_frame and i <= end_frame:
-            df.loc[i, "class"] = "mdk3"
+            df.loc[i, "class"] = "bruteforce"
         else:
             df.loc[i, "class"] = "normal"
 
         if i % 10000 == 0:
             print("Packet n°", i, "done")
+            df.to_csv("Données\\bruteforce_12min.csv", mode='a', header=False)
+            df = pd.DataFrame(columns=["packet_length", "header_length", "subtype", "duration", "datarate", "class"])
 
     # Close the capture object
     cap.close()
@@ -55,5 +66,3 @@ if __name__ == '__main__':
     t = threading.Thread(target=capture_packets)
     t.start()
     t.join()
-    df.to_csv("Données\mdk3_1h20.csv")
-    # df.to_csv("Données\\bruteforce_20min.csv")
