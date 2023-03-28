e977b00c545e90af2869c251f69d4bdf4c76876a
valent33 <v.porchet@outlook.fr>
2023-03-28 14:29:14 -0400

    classification finale

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pyshark
 import threading
 import pandas as pd
 
-path = "Données\mdk3_10min"
+path = "Données\\bruteforce_test"
 
 df = pd.DataFrame(columns=["packet_length", "header_length", "subtype", "duration", "datarate", "class"])
 # write header to csv
@@ -12,27 +12,13 @@ df.to_csv(path + ".csv", mode='w', header=True)
 def capture_packets():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-
-    # cap = pyshark.FileCapture('Données\mdk3_1h20.pcap')#, display_filter='frame.number > 170000')
-    
-    # # frame 177229 attack started
-    # start_frame = 177229
-    # # frame 184365 attack terminated
-    # end_frame = 184365
-    
-    # cap = pyshark.FileCapture('Données\\bruteforce_12min.pcap')
-
-    # # frame  attack started
-    # start_frame = 48794
-    # # frame  attack terminated
-    # end_frame = 166490
     
     cap = pyshark.FileCapture(path + '.pcap')
 
     # frame  attack started
-    start_frame = 26377
+    start_frame = 6187
     # frame  attack terminated
-    end_frame = 77789
+    end_frame = 47848
 
     global df
 
@@ -52,7 +38,7 @@ def capture_packets():
         df.loc[i, "datarate"] = p.wlan_radio.data_rate # Mbps
         
         if i >= start_frame and i <= end_frame:
-            df.loc[i, "class"] = "mdk3"
+            df.loc[i, "class"] = "bruteforce"
         else:
             df.loc[i, "class"] = "normal"
 
